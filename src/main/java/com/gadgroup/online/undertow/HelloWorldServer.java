8dd5ff94b1e7939950d8ed30a00a570f21d790c2
tunggad <tunggad.vu@googlemail.com>
2015-03-28 11:18:50 +0100

    add HelloWorldServer

---
+++ src/main/java/com/gadgroup/online/undertow/HelloWorldServer.java
@@ -0,0 +1,25 @@
+package com.gadgroup.online.undertow;
+
+import io.undertow.Undertow;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.Headers;
+
+/**
+ * Created by tvu on 28.03.15.
+ */
+public class HelloWorldServer {
+
+    public static void main(final String[] args) {
+        Undertow server = Undertow.builder()
+                .addHttpListener(8080, "localhost")
+                .setHandler(new HttpHandler() {
+                    @Override
+                    public void handleRequest(final HttpServerExchange exchange) throws Exception {
+                        exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
+                        exchange.getResponseSender().send("Hello World");
+                    }
+                }).build();
+        server.start();
+    }
+}
